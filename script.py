3a30fc7ae3f5a7deb520b8cececce24d970e75d9
Edmund To <edmund.to@industrieit.com>
2017-05-15 23:38:58 +0800

    completed

--- script.py
+++ script.py
@@ -43,8 +43,54 @@ dc_listings = dc_listings.loc[np.random.permutation(len(dc_listings))]
 dc_listings = dc_listings.sort_values('distance')
 print(dc_listings.iloc[0:10]['price'])
 
+#6
+# Remove the commas (,) and dollar sign characters ($) from the price column:
+# Use the str accessor so we can apply string methods to each value in the column followed by the string method replace to replace all comma characters with the empty character: stripped_commas = dc_listings['price'].str.replace(',', '')
+# Repeat to remove the dollar sign characters as well.
+# Convert the new Series object containing the cleaned values to the float datatype and assign back to the price column in dc_listings.
+# Calculate the mean of the first 5 values in the price column and assign to mean_price.
+# Use the print function or the variable inspector below to display mean_price.
+
+stripped_commas = dc_listings['price'].str.replace(',', '')
+stripped_dollars = stripped_commas.str.replace('$', '')
+dc_listings['price'] = stripped_dollars.astype('float')
+mean_price = dc_listings.iloc[0:5]['price'].mean()
+print(mean_price)
 
 
 
+#7
+# Write a function named predict_price that can use the k-nearest neighbors machine learning technique to calculate the suggested price for any value for accommodates. This function should:
+# Take in a single parameter, new_listing, that describes the number of bedrooms.
+# Assign dc_listings to a new Dataframe named temp_df so we aren't constantly modifying the original dataset each time we call the function.
+# Calculate the distance between each value in the accommodates column and the new_listing value that was passed in. Assign the resulting Series object to the distance column in temp_df.
+# Sort temp_df by the distance column and select the first 5 values in the price column. Don't randomize the ordering of temp_df.
+# Calculate the mean of these 5 values and use that as the return value for the entire predict_price function.
+# Use the predict_price function to suggest a price for a living space that:
+# accommodates 1 person, assign the suggested price to acc_one.
+# accommodates 2 people, assign the suggested price to acc_two.
+# accommodates 4 people, assign the suggested price to acc_four.
+# Brought along the changes we made to the `dc_listings` Dataframe.
+dc_listings = pd.read_csv('dc_airbnb.csv')
+stripped_commas = dc_listings['price'].str.replace(',', '')
+stripped_dollars = stripped_commas.str.replace('$', '')
+dc_listings['price'] = stripped_dollars.astype('float')
+dc_listings = dc_listings.loc[np.random.permutation(len(dc_listings))]
+
+def predict_price(new_listing):
+    temp_df = dc_listings
+    temp_df['distance'] = temp_df['accommodates'].apply(lambda x: np.abs(x - new_listing))
+    temp_df = temp_df.sort_values('distance')
+    nearest_neighbors = temp_df.iloc[0:5]['price']
+    predicted_price = nearest_neighbors.mean()
+    return(predicted_price)
+
+acc_one = predict_price(1)
+acc_two = predict_price(2)
+acc_four = predict_price(4)
+print('This is the predicted price for one, two and four bedrooms')
+print(acc_one)
+print(acc_two)
+print(acc_four)
 
 
